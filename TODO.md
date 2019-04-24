fa558869c7639b55896a4f624733fb507c60dcad
Andrew McPherson <mcphandr@amazon.com>
2019-04-24 13:51:22 -0700

    Added a better title screen.

--- TODO.md
+++ TODO.md
@@ -1,4 +1,7 @@
 Before the jam:
 - Figure out how to bundle Electron.
 - Manage sounds and music.
-- Create a title screen.
+
+Eventually:
+- Add keyboard support to the title screen.
+- Add url/history/back-button support to the screens.
--- source/views/screens/TitleScreen.view.js
+++ source/views/screens/TitleScreen.view.js
@@ -1,4 +1,5 @@
 import Preact from "preact"
+import Index from "index"
 
 import "views/screens/TitleScreen.view.less"
 
@@ -6,8 +7,34 @@ export default class TitleScreen {
     render() {
         return (
             <div class="TitleScreen">
-                Welcome!
+                <section class="MenuSection">
+                    <div class="Title">
+                        <h1>Game Title!!</h1>
+                        <h2>A subtitle about the game.</h2>
+                    </div>
+                    <div class="Links">
+                        <Link label="Play" screen="GameScreen"/>
+                        <Link label="Options" screen="OptionsScreen"/>
+                        <Link label="Feedback" screen="FeedbackScreen"/>
+                    </div>
+                </section>
+                <section class="HeroSection"/>
             </div>
         )
+    }
+}
+
+class Link {
+    render() {
+        return (
+            <div class="Link" onClick={this.onClick}>
+                {this.props.label}
+            </div>
+        )
+    }
+    get onClick() {
+        return (event) => {
+            Index.model.screen = this.props.screen
+        }
     }
 }
