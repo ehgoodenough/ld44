bfc5d419aacfe9b0288b0cda13a287215925c7cc
Andrew McPherson <mcphandr@amazon.com>
2019-04-26 11:17:58 -0700

    Added modal exit behavior to the pause menu.

--- source/views/screens/GameScreen.view.js
+++ source/views/screens/GameScreen.view.js
@@ -7,21 +7,33 @@ import Link from "views/Link.view.js"
 export default class GameScreen {
     render() {
         return (
-            <div class="GameScreen">
+            <div class="GameScreen" onClick={this.onClick}>
                 <PauseMenu/>
             </div>
         )
+    }
+    get onClick() {
+        return (event) => {
+            if(Index.model.game.isPaused == true) {
+                Index.model.game.isPaused = false
+            }
+        }
     }
 }
 
 class PauseMenu {
     render() {
         return (
-            <div class="PauseMenu" isPaused={Index.model.game.isPaused}>
+            <div class="PauseMenu" isPaused={Index.model.game.isPaused} onClick={this.onClick}>
                 <h1>Paused!!!</h1>
                 <Link label="Return to Titlescreen?" screen="TitleScreen"/>
                 <small>Don't worry, your progress will be saved!</small>
             </div>
         )
+    }
+    get onClick() {
+        return (event) => {
+            event.stopPropagation()
+        }
     }
 }
