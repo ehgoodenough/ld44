b23e55fc74092fefcdbacdf1f255365278a4d689
Andrew McPherson <mcphandr@amazon.com>
2019-04-26 21:46:01 -0700

    Made the turret tween its rotation.

--- source/models/Turret.js
+++ source/models/Turret.js
@@ -7,29 +7,43 @@ export default class Turret {
         this.width = 1
         this.height = 1
 
-        this.position = {"x": 8, "y": 1}
+        this.position = {"x": 16/2, "y": 9/2}
         this.rotation = Math.PI / 2
         this.velocityRotation = 0
         this.targetRotation = 0
 
         this.stack = 5
 
-        this.color = "#A52F22"
-
         this.timer = 2
         this.shots = 3
         this.isShooting = false
+        this.isDoneShooting = false
+
+        this.deceleration = 0.25
     }
     update(delta) {
-
-        this.targetRotation = getRotation(this.position, Index.model.game.player.position)
-        // if(this.rotation < this.targetRotation) {
-        //     this.velocityRotation = Math.PI / 16
-        // }
-
         if(this.isShooting == false) {
-            // this.rotation += this.velocityRotation
-            this.rotation = this.targetRotation
+            this.targetRotation = getRotation(this.position, Index.model.game.player.position)
+            if(this.rotation < this.targetRotation) {
+                this.velocityRotation += (this.targetRotation - this.rotation) / 32
+            }
+            if(this.rotation > this.targetRotation) {
+                this.velocityRotation -= (this.rotation - this.targetRotation) / 32
+            }
+
+            this.rotation += this.velocityRotation
+
+            this.velocityRotation -= this.velocityRotation * this.deceleration
+
+            if(this.velocityRotation > +1) {
+                this.velocityRotation = +1
+            }
+            if(this.velocityRotation < -1) {
+                this.velocityRotation = -1
+            }
+
+            // TODO: Don't spin around when I go clockwise around you.
+            // TODO: Don't jitter.
         }
 
         this.timer -= delta.s
@@ -48,12 +62,27 @@ export default class Turret {
                     "position": this.position,
                     "affiliation": "baddies",
                 }))
+            } else if(this.isDoneShooting == false) {
+                this.isDoneShooting = true
+                this.timer = 0.5
             } else {
                 this.isShooting = false
+                this.isDoneShooting = false
                 this.shots = 3
                 this.timer = 2
             }
         }
+    }
+    get color() {
+        if(this.isShooting) {
+            if(this.shots > 0
+            && this.shots < 3
+            && this.timer * 100000 % 2) {
+                return "#FFFFFF"
+            }
+            return "#7B2319"
+        }
+        return "#A52F22"
     }
 }
 
